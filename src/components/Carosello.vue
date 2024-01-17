abf5bb94a7c33bb1f3272e6afea5e429439360ea
gianlucanied <gianluca.nieddu96@gmail.com>
2024-01-17 15:24:46 +0100

    Add: carosello.vue

---
+++ src/components/Carosello.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: 'Carosello'
+}
+</script>
+
+<template></template>
+
+<style lang="scss" scoped>
+//Scss relativo alla sola sezione di header
+@use '../styles/partials/variables' as *;
+</style>
