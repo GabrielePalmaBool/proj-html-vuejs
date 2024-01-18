75a7bef2f0b0612f72f19e64db938bbdded81a53
Gabriele <gabriele.palma.bool@gmail.com>
2024-01-18 11:22:14 +0100

    prova

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,13 @@ import Appheader from './components/Appheader.vue'
 
 import Appfooter from './components/Appfooter.vue'
 
-import Carosello from './components/Carosello.vue'
-
 import { RouterView } from 'vue-router'
 
 export default {
   components: {
     Appheader,
     Appfooter,
-    Carosello,
+
     RouterView
   }
 }
